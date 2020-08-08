3956a4b50a0c9b174bbeee2bfbed383fd300e911
Jieranxinxin <jieran0530@163.com>
2020-08-08 21:17:09 +0800

    Java

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/5a/807f215d1053001a11dde9af95394bf2
@@ -0,0 +1,8 @@
+package com.xinxin.java;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("helloworld!");
+	}
+
+}
--- HelloWorld/src/com/test/HelloWorld.java
+++ HelloWorld/src/com/test/HelloWorld.java
@@ -11,7 +11,22 @@ public class HelloWorld {
 		System.out.println("Hello World4!");
 		System.out.println("Hello World5!");
 		System.out.println("Hello World6!");
+		//<<<<<<< HEAD
+		System.out.println("Hello zhang!");
 		System.out.println("Hello World7!");
+		System.out.println("Hello World8!");
+		System.out.println("Hello zhengqiuyue!");
+		System.out.println("Hello ÕÅ¼ÑÄÐ!");
+		System.out.println("Hello yudi!");
+		System.out.println("Hello jzy!");
+		System.out.println("Hello jzy!");
+		System.out.println("Hello panfan!");
+		System.out.println("Hello guojianlong!");
+		System.out.println("Hello jqz");
+		System.out.println("Hello jzy!");
+	}
+		//>>>>>>> branch 'master' of https://gitee.com/melody924/HelloWorld.git
 	}
 
-}
+
+
